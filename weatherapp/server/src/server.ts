f7e173873d5a45ab55e6e00bd174607a7c6afcc2
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-02-18 23:21:36 -0500

    cleaning house

--- weatherapp/server/src/server.ts
+++ weatherapp/server/src/server.ts
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
 
 
@@ -10,11 +8,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
